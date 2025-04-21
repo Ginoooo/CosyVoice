7e40b11cf2489ef21394a12979d4d304051fff6e
Ginooo <a115379624@qq.com>
2025-04-21 10:17:04 +0800

    # 提交

--- api.py
+++ api.py
@@ -33,7 +33,7 @@ import base64
 from modelscope import snapshot_download
 snapshot_download('iic/CosyVoice2-0.5B', local_dir='pretrained_models/CosyVoice2-0.5B')
 snapshot_download('iic/CosyVoice-300M-SFT', local_dir='pretrained_models/CosyVoice-300M-SFT')
-
+snapshot_download('iic/CosyVoice-ttsfrd', local_dir='pretrained_models/CosyVoice-ttsfrd')
 
 
 '''
